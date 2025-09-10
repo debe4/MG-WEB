6c888a804c01a0be13df4c4e36e5bcc377b781de
MG_suzuya <78275546+debe4@users.noreply.github.com>
2025-09-10 13:19:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const clickSound = document.getElementById('click-sound');
+
+// Global click sound
+document.addEventListener('click', () => {
+  clickSound.currentTime = 0;
+  clickSound.play();
+});
+
+function openTab(evt, tabId) {
+  const contents = document.querySelectorAll('.tab-content');
+  contents.forEach(c => c.classList.remove('active'));
+
+  const links = document.querySelectorAll('.tab-link');
+  links.forEach(l => l.classList.remove('active'));
+
+  document.getElementById(tabId).classList.add('active');
+  evt.currentTarget.classList.add('active');
+}
+
+function copyDiscord() {
+  const discord = document.getElementById("discord").innerText;
+  navigator.clipboard.writeText(discord).then(() => {
+    alert("Copied: " + discord);
+  });
+}
